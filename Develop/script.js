66acc62f7d2affa8cfa8b6314248e5ef8ad0330b
jessicap5159 <jessicaj5159@gmail.com>
2021-02-17 10:13:19 -0800

    added validation for numbers

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,25 @@ var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 var lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numberArray = [0,1,2,3,4,5,6,7,8,9];
 var specialArray = ["!","@","#","$","%","^","&","*","?"]; 
+var passwordText = document.querySelector("#password");
 
 
  var generatePassword = function() {
   var promptLength = prompt("How many characters do you want in your password? Enter a number between 8 and 128, inclusive.");
-    // validate prompt answer 
+  if (isNaN(promptLength)){
+  return("Please provide the input as a number. Click the red button to try again.");
+  }
+  if (promptLength % 1 != 0)
+  return("Please provide the input as a whole number. Click the red button to try again.");
+  if (promptLength < 8)
+  return("Please provide the input as a whole number. Click the red button to try again.");
+  if (promptLength > 128)
+  return("Please provide the input as a whole number. Click the red button to try again.");
+
   var upperSelection = confirm("Would you like to use uppercase characters? Click 'okay' for yes, and 'cancel' for no.");
   var lowerSelection = confirm("Would you like to use lowercase characters? Click 'okay' for yes, and 'cancel' for no.");
   var specialSelection = confirm("Would you like to use special characters? Click 'okay' for yes, and 'cancel' for no.");
+  var numberSelection = confirm("Would you like to use numbers? Click 'okay' for yes, and 'cancel' for no.");
  }
 
     // validate prompt answer
@@ -20,9 +31,9 @@ var specialArray = ["!","@","#","$","%","^","&","*","?"];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword() {  
+  passwordText.value = "";
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
