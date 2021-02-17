1b82eacd70a253541a7b5e7d025bfd5b4024e8f7
jessicap5159 <jessicaj5159@gmail.com>
2021-02-17 09:44:24 -0800

    added arrays and confirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,19 @@
 // Assignment code here
-var passLength = function() {
+
+var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowerArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numberArray = [0,1,2,3,4,5,6,7,8,9];
+var specialArray = ["!","@","#","$","%","^","&","*","?"]; 
+
+
+ var generatePassword = function() {
   var promptLength = prompt("How many characters do you want in your password? Enter a number between 8 and 128, inclusive.");
     // validate prompt answer 
+  var upperSelection = confirm("Would you like to use uppercase characters? Click 'okay' for yes, and 'cancel' for no.");
+  var lowerSelection = confirm("Would you like to use lowercase characters? Click 'okay' for yes, and 'cancel' for no.");
+  var specialSelection = confirm("Would you like to use special characters? Click 'okay' for yes, and 'cancel' for no.");
+ }
 
-    // if (passLength === "" || passLength === null || !isNaN(passLength)) {
-    //   window.alert("Response not valid, please try again.");
-    //   // use return to call it again and stop the rest of this function from running
-    //   return passLength();
-    // }
-    // validate string length also
-
-
-var charType = prompt("Which type(s) of characters do you want? Please choose at least one: uppercase, lowercase, numeric, or special.")
     // validate prompt answer
 
 // Get references to the #generate element
@@ -28,3 +30,31 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
